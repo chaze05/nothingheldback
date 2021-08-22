6101d6a7f3c1f773cc56819acc66c81408c2b50b
rani <rani@spin.cw>
2021-08-23 00:04:09 +0800

    showmore

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 $(function(){
+    // masonry
     $('.stories').masonry({
         itemSelector: '.storyBox',
         columnWidth: 326,
         gutter:12,
         horizontalOrder: true
     });
+
+    // submenu
     $('.wdcaret').on('click', function(){
         event.preventDefault()
         if($(this).parent().find('.subMenu').is(":visible")){
@@ -15,6 +18,23 @@ $(function(){
         }
 
     })
+
+
+    // read more
+    $('.showMore').on('click', function(e){
+        e.preventDefault();
+        if( $(this).parent().find('p').hasClass('showmore')){
+            $(this).parent().find('p').removeClass('showmore')
+            $(this).text("Show more...");
+        }else{
+             $(this).parent().find('p').addClass('showmore');
+            $(this).text("Show less...");
+        }
+    })
+
+
+    // sort options
+    
     $('.sorter').on('click', function(){
         $(this).parent().find('.options').fadeIn();
     })
