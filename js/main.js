683090509d230b720ec21227330dbbde0b6598f9
rani <rani@spin.cw>
2021-08-22 23:54:08 +0800

    additional

--- js/main.js
+++ js/main.js
@@ -5,5 +5,22 @@ $(function(){
         gutter:12,
         horizontalOrder: true
     });
+    $('.wdcaret').on('click', function(){
+        event.preventDefault()
+        if($(this).parent().find('.subMenu').is(":visible")){
+            $(this).parent().find('.subMenu').slideUp();
+        }else{
+            $('.subMenu').slideUp();
+            $(this).parent().find('.subMenu').slideDown();
+        }
 
+    })
+    $('.sorter').on('click', function(){
+        $(this).parent().find('.options').fadeIn();
+    })
+    $('.sortBy .options span').on('click',function(){
+        var sort =  $(this).data("option");
+        $(this).parents('.sortBy').find('.selected').text(sort);
+        $(this).parent().fadeOut();
+    })
 });
